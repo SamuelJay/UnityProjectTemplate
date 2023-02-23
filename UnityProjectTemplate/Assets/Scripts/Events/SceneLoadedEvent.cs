9cc041fb5b1be1718f95b09fb124191d607de586
Samuel Piggott <sjpiggott93@gmail.com>
2023-02-23 10:53:43 +0700

    Develop: SceneLoadingManager and SceneLoaded event

---
+++ UnityProjectTemplate/Assets/Scripts/Events/SceneLoadedEvent.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadedEvent : BaseEvent
+{
+    public Scene scene { get; private set; }
+    public LoadSceneMode mode { get; private set; }
+
+    public SceneLoadedEvent(Scene sceneIn, LoadSceneMode modeIn)
+    {
+        scene = sceneIn;
+        mode = modeIn;
+    }
+}
--- UnityProjectTemplate/Assets/Scripts/Managers/SceneLoadingManager.cs
+++ UnityProjectTemplate/Assets/Scripts/Managers/SceneLoadingManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoadingManager : Manager
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Setup(AppManager appManagerIn)
     {
-        
+        base.Setup(appManagerIn);
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        
+        TriggerEvent<SceneLoadedEvent>(new SceneLoadedEvent(scene, mode));
+    }
+
+    public void LoadScene(string name, LoadSceneMode mode)
+    {
+        SceneManager.LoadScene(name, mode);
+    }
+
+    public void UnLoadScene(string name)
+    {
+        SceneManager.UnloadSceneAsync(name);
+    }
+
+    public void SetActiveScene(string name)
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(name));
     }
 }
