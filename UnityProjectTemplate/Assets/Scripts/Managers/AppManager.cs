1f54474470e8a1a6b4cc971962c79c3b88e97b31
Samuel Piggott <sjpiggott93@gmail.com>
2023-02-23 10:49:27 +0700

    Develop Instantiating all the managers getting the manager components and calling setup

--- UnityProjectTemplate/Assets/Scripts/Managers/AppManager.cs
+++ UnityProjectTemplate/Assets/Scripts/Managers/AppManager.cs
@@ -4,9 +4,57 @@ using UnityEngine;
 
 public class AppManager : Manager
 {
+    public EventManager eventManager { get; private set; }
+    public SceneLoadingManager sceneLoadingManager { get; private set; }
+    public UIManager uiManager { get; private set; }
+    public MainMenuManager mainMenuManager { get; private set; }
+    public GameManager gameManager { get; private set; }
+
     [SerializeField] private GameObject eventManagerPrefab;
     [SerializeField] private GameObject sceneLoadingManagerPrefab;
     [SerializeField] private GameObject uiManagerPrefab;
     [SerializeField] private GameObject mainMenuManagerPrefab;
     [SerializeField] private GameObject gameManagerPrefab;
+
+    private void Awake()
+    {
+        Setup(this);
+    }
+
+    public override void Setup(Manager manager)
+    {
+        base.Setup(manager);
+        GameObject eventManagerObject = Instantiate(eventManagerPrefab);
+        GameObject sceneLoadingManagerObject = Instantiate(sceneLoadingManagerPrefab);
+        GameObject uiManagerObject = Instantiate(uiManagerPrefab);
+
+        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(eventManagerObject);
+        DontDestroyOnLoad(sceneLoadingManagerObject);
+        DontDestroyOnLoad(uiManagerObject);
+
+        eventManager = eventManagerObject.GetComponent<EventManager>();
+        sceneLoadingManager = sceneLoadingManagerObject.GetComponent<SceneLoadingManager>();
+        uiManager = uiManagerObject.GetComponent<UIManager>();
+
+        eventManager.Setup(this);
+        sceneLoadingManager.Setup(this);
+        uiManager.Setup(this);
+
+        
+    }
+
+    private void SetupMainMenu()
+    {
+        GameObject mainMenuManagerObject = Instantiate(mainMenuManagerPrefab);
+        mainMenuManager = mainMenuManagerObject.GetComponent<MainMenuManager>();
+        mainMenuManager.Setup(this);
+    }
+
+    private void SetupGame()
+    {
+        GameObject gameManagerObject = Instantiate(gameManagerPrefab);
+        gameManager = gameManagerObject.GetComponent<GameManager>();
+        gameManager.Setup(this);
+    }
 }
